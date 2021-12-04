14c6ac2e130b0387cfae973acc8ee9371dc862c5
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-12-04 21:25:44 +0100

    image slider with react-slick

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,72 @@
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+
+function ImgSlider() {
+
+  let settings = {
+    dots: true,
+    Infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  } 
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src='/images/slider-badging.jpg'/>
+      </Wrap>
+      <Wrap>
+        <img src='/images/slider-badging.jpg'/>
+      </Wrap>
+    </Carousel>
+  )
+}
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: white;
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: visible;
+  }
+
+  button {
+    z-index: 1;
+  } 
+`
+
+const Wrap = styled.div`
+
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    box-shadow: rgb(0, 0, 0/69%) 0px 26px 30px --10px, rgb(0, 0, 0/73%) 0px 16px 10px -10px;
+    border: 4px solid transparent;
+    transition-duration: 300ms;
+
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+
+    }
+  }
+
+`
+
+export default ImgSlider
